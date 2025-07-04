7bbcf571c50f2aadda39e2b9092fbb832365680e
Shahzad <shaizcodes@gmail.com>
2025-07-04 16:16:44 +0500

    try CSS module
    + apply the same Tailwind CSS styles via CSS module
    
    * CSS modules are for reusable module specific components.
    
    * It helps avoid duplicate imports as well as style conflicts as unique classes are assigned module wise.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import styles from '@/app/ui/home.module.css';
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -7,6 +9,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"></div>
+        <div className={styles.shape}></div>
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
